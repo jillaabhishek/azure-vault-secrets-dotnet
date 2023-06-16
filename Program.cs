eb6c2866c3e77c45e70d8f2c7bfd23531e516ce7
Abhishek Jilla <jilla.abhishek94@gmail.com>
2023-06-16 07:37:23 +0000

    Azure Secrets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace dotnetcore
 {
@@ -6,7 +9,41 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "MySecret";
+            string keyVaultName = "my-vault-no-protect94";
+            var kvUri = "https://my-vault-no-protect94.vault.azure.net/";
+
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+            System.Console.WriteLine("Get Secret: " + secret.Value);
+            System.Console.Write("Enter Secret >");
+
+            string secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            System.Console.Write("  SetSecret:" );
+            System.Console.Write(" Key:" + secretName);
+            System.Console.Write("  Value: " + secretValue);
+
+            System.Console.WriteLine("GetSecret:" + secret.Value);
+
+            client.StartDeleteSecret(secretName);
+            System.Console.WriteLine("StartDeleteSecret:" + keyVaultName);
+
+            System.Console.WriteLine("GetSecret: "+secret.Value);
+
+
+
+
         }
     }
 }
